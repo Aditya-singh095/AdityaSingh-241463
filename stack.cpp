2b50d849f3d63cc9ee9ddb4e37fd5208586bc6e4
Juniortony095 <as9864813@gmail.com>
2025-11-22 11:25:40 +0530

    stack

---
+++ stack.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+class Stack
+{
+    int arr[MAX];
+    int top;
+
+public:
+    Stack() { top = -1; }
+
+    void push(int x)
+    {
+        if (top == MAX - 1)
+        {
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        arr[++top] = x;
+        cout << "Pushed " << x << endl;
+    }
+
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "Stack Underflow" << endl;
+            return;
+        }
+        cout << "Popped " << arr[top--] << endl;
+    }
+
+    void peek()
+    {
+        if (top == -1)
+        {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        cout << "Top element: " << arr[top] << endl;
+    }
+
+    void display()
+    {
+        if (top == -1)
+        {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        cout << "Stack elements: ";
+        for (int i = top; i >= 0; i--)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    Stack s;
+    int choice, value;
+
+    cout << "\n1. Push\n2. Pop\n3. Peek\n4. Display\n5. Exit\n";
+
+    
+    {
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter value to push: ";
+            cin >> value;
+            s.push(value);
+            break;
+        case 2:
+            s.pop();
+            break;
+        case 3:
+            s.peek();
+            break;
+        case 4:
+            s.display();
+            break;
+        case 5:
+            cout << "Exiting..." << endl;
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+        }
+    } while (choice != 5);
+    return 0;
+}
