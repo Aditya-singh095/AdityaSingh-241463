fb9ce9004d28b5f028a14c00f42784a55e628af6
Juniortony095 <as9864813@gmail.com>
2025-11-22 11:02:28 +0530

    Insertion

---
+++ insertion.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+ void insertion(int arr[] , int n){
+for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+
+
+ }
+int main() {
+    int size;
+    cout << "Enter number of elements: ";
+    cin >> size;
+    int arr[100];
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    insertion(arr , size );
+    return 0;
+}
